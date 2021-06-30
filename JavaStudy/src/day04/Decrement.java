5d7db00b8aa80928a807285d8cbd87993bfeaad3
HBA <HBA@192.168.1.42>
2021-06-30 23:28:33 +0300

    day04

---
+++ JavaStudy/src/day04/Decrement.java
@@ -0,0 +1,23 @@
+package day04;
+
+public class Decrement {
+
+	public static void main(String[] args) {
+		
+		 int sayi1=20;
+	        
+	        int sayi2 = sayi1--;
+	        
+	        System.out.println("post decrement sayi1 : " + sayi1); // 
+	        System.out.println("post decrement sayi2 : " + sayi2); // 
+	        
+	        
+	        System.out.println(--sayi2); // 
+	        
+	        System.out.println(sayi2--); // 
+	        
+	        System.out.println(sayi2);  // 
+		
+	}
+
+}
